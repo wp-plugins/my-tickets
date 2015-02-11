6f9b767eb682ce9cbf0d35d1491dde91ca23a732
joedolson <joedolson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-11 17:31:17 +0000

    Commit 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/my-tickets/tags/1.0.3@1087291 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gateways/PayPal.php
@@ -0,0 +1,170 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+add_action( 'mt_receive_ipn', 'mt_paypal_ipn' );
+function mt_paypal_ipn() {
+	if ( isset( $_REQUEST['mt_paypal_ipn'] ) && $_REQUEST['mt_paypal_ipn'] == 'true' ) {
+		if ( isset( $_POST['payment_status'] ) ) {
+			$options  = array_merge( mt_default_settings(), get_option( 'mt_settings' ) );
+			$receiver = ( isset( $options['mt_paypal_email'] ) ) ? strtolower( $options['mt_paypal_email'] ) : false;
+			$url      = ( $options['mt_use_sandbox'] == 'true' ) ? 'https://www.sandbox.paypal.com/webscr' : 'https://www.paypal.com/webscr';
+
+			$req = 'cmd=_notify-validate';
+			foreach ( $_POST as $key => $value ) {
+				$value = urlencode( stripslashes( $value ) );
+				$req .= "&$key=$value";
+			}
+			$args   = wp_parse_args( $req, array() );
+			$params = array(
+				'body'      => $args,
+				'sslverify' => false,
+				'timeout'   => 30,
+			);
+
+			// transaction variables to store
+			$payment_status   = $_POST['payment_status'];
+			$item_number      = $_POST['item_number'];
+			$price            = $_POST['mc_gross'];
+			$payment_currency = $_POST['mc_currency'];
+			$receiver_email   = $_POST['receiver_email'];
+			$payer_email      = $_POST['payer_email'];
+			$payer_first_name = $_POST['first_name'];
+			$payer_last_name  = $_POST['last_name'];
+			$mc_fee           = $_POST['mc_fee'];
+			$txn_id           = $_POST['txn_id'];
+			$parent           = isset( $_POST['parent_txn_id'] ) ? $_POST['parent_txn_id'] : '';
+			// paypal IPN data
+			$ipn           = wp_remote_post( $url, $params );
+			$response      = $ipn['body'];
+			$response_code = $ipn['response']['code'];
+
+			// map paypal IPN format of address to MT format
+			$address = array(
+				'street'  => isset( $_POST['address_street'] ) ? $_POST['address_street'] : '',
+				'street2' => isset( $_POST['address2'] ) ? $_POST['address2'] : '',
+				'city'    => isset( $_POST['address_city'] ) ? $_POST['address_city'] : '',
+				'state'   => isset( $_POST['address_state'] ) ? $_POST['address_state'] : '',
+				'country' => isset( $_POST['address_country_code'] ) ? $_POST['address_country_code'] : '',
+				'code'    => isset( $_POST['address_zip'] ) ? $_POST['address_zip'] : ''
+			);
+			//wp_mail( 'joe@joedolson.com', 'Shipping fields', print_r( $_POST, 1 ) );
+			$data = array(
+				'transaction_id' => $txn_id,
+				'price'          => $price,
+				'currency'       => $payment_currency,
+				'email'          => $payer_email,
+				'first_name'     => $payer_first_name,
+				'last_name'      => $payer_last_name,
+				'fee'            => $mc_fee,
+				'parent'         => $parent,
+				'status'         => $payment_status,
+				'purchase_id'    => $item_number,
+				'shipping'       => $address
+			);
+			// die conditions for PayPal
+			// if receiver email or currency are wrong, this is probably a fraudulent transaction.
+			// if no receiver email provided, that check will be skipped.
+			if ( ( $receiver && ( strtolower( $receiver_email ) != $receiver ) ) || $payment_currency != $options['mt_currency'] ) {
+				wp_mail( $options['mt_to'], __( 'Payment Conditions Error', 'my-tickets' ), __( "PayPal receiver email did not match account or payment currency did not match payment on $item_number", 'my-tickets' ) );
+				wp_die( __( "Payment conditions did not match expectations", 'my-tickets' ) );
+			}
+			mt_handle_payment( $response, $response_code, $data, $_POST );
+			// Everything's all right.
+			status_header( 200 );
+		}
+	}
+
+	return;
+}
+
+add_filter( 'mt_shipping_fields', 'mt_paypal_shipping_fields', 10, 2 );
+function mt_paypal_shipping_fields( $form, $gateway ) {
+	if ( $gateway == 'paypal' ) {
+		$search  = array(
+			'mt_shipping_street',
+			'mt_shipping_street2',
+			'mt_shipping_city',
+			'mt_shipping_state',
+			'mt_shipping_country',
+			'mt_shipping_code'
+		);
+		$replace = array( 'address1', 'address2', 'city', 'state', 'mt_shipping_country', 'zip' );
+
+		return str_replace( $search, $replace, $form );
+	}
+
+	return $form;
+}
+
+add_filter( 'mt_format_transaction', 'mt_paypal_transaction', 10, 2 );
+function mt_paypal_transaction( $transaction, $gateway ) {
+	if ( $gateway == 'paypal' ) {
+		// alter return value if desired.
+	}
+
+	return $transaction;
+}
+
+add_filter( 'mt_setup_gateways', 'mt_setup_paypal', 10, 1 );
+function mt_setup_paypal( $gateways ) {
+	$gateways['paypal'] = array(
+		'label'  => __( 'PayPal', 'my-tickets' ),
+		'fields' => array(
+			'email'       => __( "PayPal email (primary)", 'my-tickets' ),
+			'merchant_id' => __( 'PayPal Merchant ID', 'my-tickets' )
+		)
+	);
+
+	return $gateways;
+}
+
+add_filter( 'mt_gateway', 'mt_gateway_paypal', 10, 3 );
+function mt_gateway_paypal( $form, $gateway, $args ) {
+	if ( $gateway == 'paypal' ) {
+		$options        = array_merge( mt_default_settings(), get_option( 'mt_settings' ) );
+		$payment_id     = $args['payment'];
+		$handling       = ( isset( $options['mt_handling'] ) ) ? $options['mt_handling'] : 0;
+		$total          = $args['total'] + $handling;
+		$shipping       = ( $args['method'] == 'postal' ) ? 2 : 1;
+		$shipping_price = ( $args['method'] == 'postal' ) ? money_format( '%i', $options['mt_shipping'] ) : 0;
+		$use_sandbox    = $options['mt_use_sandbox'];
+		$currency       = $options['mt_currency'];
+		$merchant       = $options['mt_gateways']['paypal']['merchant_id'];
+		$form           = "
+		<form action='" . ( $use_sandbox != 'true' ? "https://www.paypal.com/cgi-bin/webscr" : "https://www.sandbox.paypal.com/cgi-bin/webscr" ) . "' method='POST'>
+		<input type='hidden' name='cmd' value='_xclick' />
+		<input type='hidden' name='business' value='" . esc_attr( $merchant ) . "' />
+		<input type='hidden' name='item_name' value='" . sprintf( __( '%s Order', 'my-tickets' ), get_option( 'blogname' ) ) . "' />
+		<input type='hidden' name='item_number' value='" . esc_attr( $payment_id ) . "' />
+		<input type='hidden' name='amount' value='" . esc_attr( $total ) . "' />
+		<input type='hidden' name='no_shipping' value='" . esc_attr( $shipping ) . "' />
+		<input type='hidden' name='shipping' value='" . esc_attr( $shipping_price ) . "' />
+		<input type='hidden' name='no_note' value='1' />
+		<input type='hidden' name='currency_code' value='" . esc_attr( $currency ) . "' />";
+		$form .= "
+		<input type='hidden' name='notify_url' value='" . mt_replace_http( add_query_arg( 'mt_paypal_ipn', 'true', esc_url( home_url() ) . '/' ) ) . "' />
+		<input type='hidden' name='return' value='" . mt_replace_http( add_query_arg( array(
+						'response_code' => 'thanks',
+						'gateway'       => 'paypal'
+					), esc_url( get_permalink( $options['mt_purchase_page'] ) ) ) ) . "' />
+		<input type='hidden' name='cancel_return' value='" . mt_replace_http( add_query_arg( 'response_code', 'cancel', esc_url( get_permalink( $options['mt_purchase_page'] ) ) ) ) . "' />";
+		/* This might be part of handling discount codes.
+		if ( $discount == true && $discount_rate > 0 ) {
+			$form .= "
+			<input type='hidden' name='discount_rate' value='$discount_rate' />";
+			if ( $quantity == 'true' ) {
+				$form .= "
+				<input type='hidden' name='discount_rate2' value='$discount_rate' />";	
+			}
+		}
+		*/
+		$form .= mt_render_field( 'address', 'paypal' );
+		$form .= "<input type='submit' name='submit' class='button' value='" . esc_attr( apply_filters( 'mt_gateway_button_text', __( 'Make Payment through PayPal', 'my-tickets' ), $gateway ) ) . "' />";
+		$form .= apply_filters( 'mt_paypal_form', '', $gateway, $args );
+		$form .= "</form>";
+	}
+
+	return $form;
+}
---
+++ mt-cart-handler.php
@@ -0,0 +1,199 @@
+<?php
+
+add_action( 'init', 'mt_handle_cart' );
+/**
+ * Handle cart submission. Receive data, create payment, delete cart if applicable, register message.
+ */
+function mt_handle_cart() {
+	$options = array_merge( mt_default_settings(), get_option( 'mt_settings' ) );
+	if ( ! isset( $_POST['mt_submit'] ) ) {
+		return;
+	} else {
+		$nonce = $_POST['_wpnonce'];
+		if ( ! wp_verify_nonce( $nonce, 'mt_cart_nonce' ) ) {
+			die;
+		}
+		if ( !isset( $_POST['mt_fname'] ) || $_POST['mt_fname'] == '' || !isset( $_POST['mt_lname'] ) || $_POST['mt_lname'] == '' || !isset( $_POST['mt_email'] ) || $_POST['mt_email'] == '' ) {
+			$url = add_query_arg( 'response_code', 'required-fields', get_permalink( $options['mt_purchase_page'] ) );
+			wp_safe_redirect( $url );
+			die;
+		}
+		$payment = mt_create_payment( $_POST );
+		if ( $payment ) {
+			// Handle custom fields added to cart form
+			do_action( 'mt_handle_custom_cart_data', $payment, $_POST );
+			if ( isset( $_POST['mt_gateway'] ) && $_POST['mt_gateway'] == 'offline' ) {
+				// if this is offline payment, we're done.
+				mt_register_message( 'payment_due', 'success' );
+				mt_delete_data( 'cart' );
+				mt_delete_data( 'payment' );
+			} else {
+				if ( !( mt_get_data( 'payment' ) ) ) {
+					mt_save_data( $payment, 'payment' );
+				}
+			}
+		} else {
+			mt_register_message( 'payment', 'error' );
+		}
+	}
+}
+
+/**
+ * Abstract function to delete data. Defaults to delete user's shopping cart.
+ *
+ * @param string $data
+ */
+function mt_delete_data( $data = 'cart' ) {
+	$unique_id = ( isset( $_COOKIE['mt_unique_id'] ) ) ? $_COOKIE['mt_unique_id'] : false;
+	if ( is_user_logged_in() ) {
+		global $current_user;
+		get_currentuserinfo();
+		update_user_meta( $current_user->ID, "_mt_user_$data", '' );
+	} else {
+		if ( $unique_id ) {
+			delete_transient( "mt_" . $unique_id . "_" . $data );
+		}
+	}
+}
+
+/**
+ * Generates new payment from POSTed data.
+ *
+ * @param array $post
+*/
+function mt_create_payment( $post ) {
+	$options = array_merge( mt_default_settings(), get_option( 'mt_settings' ) );
+	// save payment post
+	global $current_user;
+	get_currentuserinfo();
+	$purchaser = ( is_user_logged_in() ) ? $current_user->ID : 1;
+	if ( mt_get_data( 'payment' ) ) {
+		$purchase_id = mt_get_data( 'payment' );
+		$status      = 'draft';
+	} else {
+		$status      = ( isset( $post['mt_gateway'] ) && $post['mt_gateway'] == 'offline' ) ? 'publish' : 'draft';
+		$date        = date( 'Y-m-d H:i:00', current_time( 'timestamp' ) );
+		$post_title  = $post['mt_fname'] . ' ' . $post['mt_lname'];
+		$my_post     = array(
+			'post_title'   => $post_title,
+			'post_content' => json_encode( $post ),
+			'post_status'  => 'draft',
+			'post_author'  => $purchaser,
+			'post_date'    => $date,
+			'post_type'    => 'mt-payments'
+		);
+		$purchase_id = wp_insert_post( $my_post );
+	}
+	update_post_meta( $purchase_id, '_first_name', $post['mt_fname'] );
+	update_post_meta( $purchase_id, '_last_name', $post['mt_lname'] );
+	$email = $post['mt_email'];
+	update_post_meta( $purchase_id, '_email', $email );
+	$phone = ( isset( $post['mt_phone']) ) ? $post['mt_phone'] : '';
+	update_post_meta( $purchase_id, '_phone', $phone );
+	if ( is_user_logged_in() ) {
+		update_user_meta( $purchaser, 'mt_phone', $phone );
+	}
+
+	$purchased = $post['mt_cart_order'];
+	$paid      = mt_calculate_cart_cost( $purchased );
+	if ( isset( $options['mt_handling'] ) ) {
+		$paid = $paid + $options['mt_handling'];
+	}
+	update_post_meta( $purchase_id, '_total_paid', $paid );
+	$payment_status = ( $paid == 0 ) ? 'Completed' : 'Pending';
+	update_post_meta( $purchase_id, '_is_paid', $payment_status );
+	if ( is_user_logged_in() && ! is_admin() && $options['mt_members_discount'] != '' ) {
+		update_post_meta( $purchase_id, '_discount', $options['mt_members_discount'] );
+	}
+	update_post_meta( $purchase_id, '_gateway', $post['mt_gateway'] );
+	update_post_meta( $purchase_id, '_purchase_data', $purchased );
+	update_post_meta( $purchase_id, '_ticketing_method', $post['ticketing_method'] );
+	// for pushing data into custom fields
+	do_action( 'mt_save_payment_fields', $purchase_id, $post, $purchased );
+	if ( $purchase_id ) {
+		wp_update_post( array( 'ID' => $purchase_id, 'post_name' => 'mt_payment_' . $purchase_id ) );
+	}
+	$receipt_id = md5( add_query_arg( array( 'post_type' => 'mt-payments', 'p' => $purchase_id ), home_url() ) );
+	update_post_meta( $purchase_id, '_receipt', $receipt_id );
+	if ( $status == 'publish' ) {
+		wp_update_post( array( 'ID' => $purchase_id, 'post_status' => $status ) );
+	}
+
+	return $purchase_id;
+}
+
+/**
+ * Generates tickets for purchase.
+ *
+ * @param $purchase_id
+ * @param bool $purchased
+ */
+function mt_create_tickets( $purchase_id, $purchased = false ) {
+	$purchased = ( $purchased ) ? $purchased : get_post_meta( $purchase_id, '_purchase_data', true );
+	foreach ( $purchased as $event_id => $purchase ) {
+		$registration = get_post_meta( $event_id, '_mt_registration_options', true );
+		add_post_meta( $purchase_id, '_purchased', array( $event_id => $purchase ) );
+		add_post_meta( $event_id, '_purchase', array( $purchase_id => $purchase ) );
+		add_post_meta( $event_id, '_receipt', $purchase_id );
+		foreach ( $purchase as $type => $ticket ) {
+			// add ticket hash for each ticket
+			$count                                   = $ticket['count'];
+			$price                                   = $ticket['price'];
+			$sold                                    = $registration['prices'][ $type ]['sold'];
+			$new_sold                                = $sold + $count;
+			$registration['prices'][ $type ]['sold'] = $new_sold;
+			update_post_meta( $event_id, '_mt_registration_options', $registration );
+			for ( $i = 0; $i < $count; $i ++ ) {
+				$ticket_id = mt_generate_ticket_id( $purchase_id, $type, $i, $price );
+				add_post_meta( $event_id, '_ticket', $ticket_id );
+				update_post_meta( $event_id, '_' . $ticket_id, array( 'type'        => $type,
+				                                                      'price'       => $price,
+				                                                      'purchase_id' => $purchase_id
+					) );
+			}
+		}
+	}
+}
+
+/**
+ * Generates the ticket ID from purchase ID, ticket type, number of ticket purchased of that time, and price.
+ *
+ * @param $purchase_id
+ * @param $type
+ * @param $i
+ * @param $price
+ *
+ * @return string
+ */
+function mt_generate_ticket_id( $purchase_id, $type, $i, $price ) {
+	// hash data
+	$hash = md5( $purchase_id . $type . $i . $price );
+	// reduce to 13 chars
+	$hash = substr( $hash, 0, 12 );
+	// seed with $type substring & ticket type ID
+	$hash = substr( $type, 0, 2 ) . $hash . zeroise( $i, 4 );
+
+	return $hash;
+}
+
+/**
+ * Calculates cost of cart. (Actual cost, after discounts.)
+ *
+ * @param $purchased
+ *
+ * @return float
+ */
+function mt_calculate_cart_cost( $purchased ) {
+	$total = 0;
+	foreach ( $purchased as $event_id => $tickets ) {
+		$prices = mt_get_prices( $event_id );
+		if ( $prices ) {
+			foreach ( $tickets as $type => $ticket ) {
+				$price = $prices[ $type ]['price'] * $ticket['count'];
+				$total = $total + $price;
+			}
+		}
+	}
+
+	return round( $total, 2 );
+}
