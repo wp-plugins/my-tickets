d60981eb2d6899463db252a4050fcec2de759e51
joedolson <joedolson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-26 22:00:48 +0000

    Fix QR Code generation; commit 1.2.5
    git-svn-id: https://plugins.svn.wordpress.org/my-tickets/tags/1.2.5@1231530 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mt-events.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Get data about events for use in admin or public event data
+ */
+
+/* Front end function */
+function mt_check_status( $event_id ) {
+	// check whether event can be registered for
+}
